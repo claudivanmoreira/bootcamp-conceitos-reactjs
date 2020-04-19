bd45d11e0dc696e463b7427b143ce1bcbe0980e8
Claudivan Moreira <claudivan.moreira@ctis.com.br>
2020-04-19 09:00:17 -0300

    [ReactJS] - Adicao, Remocao e Listagem de repositorios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 import "./styles.css";
 
+
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    api.post('/repositories', {
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    }).then((response) => {
+      setRepositories([...repositories, response.data]);
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`).then((response) => {
+      setRepositories(repositories.filter(repo => repo.id !== id));
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+      { repositories.map(repo =>  
+        <li key={repo.id}>
+          {repo.title}
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+      )}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
